7d219dbd717f9f7bd3dbd26f238b12d3a6555cac
Kim - leo <ochell2@naver.com>
2021-11-05 14:06:44 +0900

    주석 처리

--- project1/DetailViewController.swift
+++ project1/DetailViewController.swift
@@ -1,7 +1,7 @@
 //
 //  DetailViewController.swift
 //  project1
-//
+//  Cocoa Touch Class
 //  Created by 김승현 on 2021/08/26.
 //
 
@@ -14,7 +14,10 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //The title of the selectedImage -> the name of the selected image
         title = selectedImage
+        
+        //In DetailViewController the image name is nornal, not big like the title of ViewController.
         navigationItem.largeTitleDisplayMode = .never
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
@@ -26,17 +29,20 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    /**
+    When tapping the selectedImage screen, Swift hides the bars above.
+    Tapping again, it will show the bars again.
+     */
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = true
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
     
+    //Share button
     @objc func shareTapped() {
         guard let image = imageView.image?.jpegData(compressionQuality: 0.0) else {
             print("No image found")
--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -5,21 +5,45 @@
 //  Created by 김승현 on 2021/08/25.
 //
 
-import UIKit
+import UIKit    //This file will reference the iOS user interface toolkit.
 
 class ViewController: UITableViewController {
+    /**
+     creating a new screen of data called ViewController, based on UITableViewController.
+     When you see a data type that starts with "UI", it means it comes from UIKit.
+     + UIViewController is Apple's default screen type, which is empty and white until we change it.
+     
+     */
 
     var pictures = [String]()
     
     override func viewDidLoad() {
+        /**
+        viewDidLoad() is called by UIKit when the screen has loaded, and is ready for you to customize.
+        */
         super.viewDidLoad()
         
+        //The title of the tableView
         title = "Storm Viewer"
+        //Making the title bigger and in DetailViewController the image titles are also big.
         navigationController?.navigationBar.prefersLargeTitles = true
         
         let fm = FileManager.default
+        /**
+         It declares a constant called fm and assigns it the value returned by FileManager.default.
+         This is a data type that lets us work with the filesystem, and in our case we'll be using it to look for files.
+         */
         let path = Bundle.main.resourcePath!
+        /**
+         It declares a constant called path that is set to the resource path of our app's bundel.
+         A bundle is a directory containing our compiled program and all our assets.
+         This line says "tell me where I can find all those images I added to my app."
+         */
         let items = try! fm.contentsOfDirectory(atPath: path)
+        /**
+         It declares a third constant called items that is set to the contents of the directory at a path, the one that was returned by the line before.
+         The items constant will be an array of strings containing filenames.
+         */
         
         for item in items {
             if item.hasPrefix("nssl") {
@@ -30,20 +54,23 @@ class ViewController: UITableViewController {
         print(pictures)
     }
 
-
+    //Showing lots of rows in tableView.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
-    
+    //Dequeuing cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
-    
+    //Typecasting DetailViewController to storyboard
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //2: success! Set its selectedImage property
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
+            
+            //3: now push it onto the navigation controller
             navigationController?.pushViewController(vc, animated: true)
         }
     }
